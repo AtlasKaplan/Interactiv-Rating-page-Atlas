51b980f8f25362bb430764b96eb33c6587f26423
Atlas_Kaplan <venatic.kaplan@gmail.com>
2023-05-11 08:41:25 -0600

    final style

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberContainer = document.querySelector(".rating-numbers");
+const rateNumber = document.querySelector(".result");
+const submitBtn = document.querySelector(".submit-btn");
+const ratingState = document.querySelector(".rate-content");
+const thankyouState = document.querySelector(".thanks-content");
+
+
