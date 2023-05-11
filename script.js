da3fb33706eb6bda166404ef41ae2c353ddef147
Atlas_Kaplan <venatic.kaplan@gmail.com>
2023-05-11 08:59:31 -0600

    js comleted

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const ratingState = document.querySelector(".rate-content");
 const thankyouState = document.querySelector(".thanks-content");
 
 
+numberContainer.addEventListener('click', event=>{
+    const numberSelected = event.target.innerText;
+    rateNumber.innerText=numberSelected;
+    if(numberSelected>0 || numberSelected<=5){
+        submitBtn.addEventListener('click', ()=>{
+            ratingState.style.display = 'none';
+            thankyouState.style.display = 'flex';
+        });
+    }
+});
